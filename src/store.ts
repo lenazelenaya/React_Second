944d26a0725bcd12cc7b1aaabb257a2e26a497a0
lenazelenaya <lena.zelenaya@gmail.com>
2020-07-25 15:04:48 +0300

    manage store

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,11 @@
 import rootReducer from "./reducers/index";
-import { createStore } from "redux";
+import { createStore } from 'redux';
+import { Store } from "./types/store";
 
-export default createStore(rootReducer);
+const initialState = {
+
+};
+
+const enh = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+
+export default createStore(rootReducer, initialState, enh);
